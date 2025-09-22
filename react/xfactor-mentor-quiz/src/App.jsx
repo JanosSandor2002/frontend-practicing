7aeee1ccea929b321a32fe18a1e989377a42f743
JanosSandor2002 <sandorj2002@gmail.com>
2025-09-22 14:10:02 +0200

    x factor readied

--- react/xfactor-mentor-quiz/src/App.jsx
+++ react/xfactor-mentor-quiz/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Details from './components/Details';
+import Footer from './components/Footer';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 
 const App = () => {
+  const showResult = () => {
+    // A Result div megjelenítése
+    document.getElementById('result-container').style.display = 'block';
+  };
   return (
     <>
       <Navbar />
+      <Details />
+      <Quiz />
+      <Footer />
     </>
   );
 };
--- react/xfactor-mentor-quiz/src/components/Navbar.jsx
+++ react/xfactor-mentor-quiz/src/components/Navbar.jsx
@@ -4,11 +4,13 @@ import userImage from '../images/user.png';
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src={navlogo} alt='logo' id='navbar-logo' />
-      <h1>X faktor mentorok Quiz {new Date().getFullYear()}</h1>
-      <a href='https://my-personal-website-janossandor.netlify.app/'>
-        <img src={userImage} alt='' />
-      </a>
+      <div className='navbaritems'>
+        <img src={navlogo} alt='logo' id='navbar-logo' />
+        <h1>X faktor mentorok Quiz {new Date().getFullYear()}</h1>
+        <a href='https://my-personal-website-janossandor.netlify.app/'>
+          <img src={userImage} alt='' />
+        </a>
+      </div>
     </div>
   );
 };
