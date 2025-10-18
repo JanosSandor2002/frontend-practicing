29b692fd3e182babb7938cb038c76224f86045f7
JanosSandor2002 <sandorj2002@gmail.com>
2025-10-18 16:27:58 +0200

    continuing web tech

--- webtech_1/beadando/script.js
+++ webtech_1/beadando/script.js
@@ -1,3 +1,53 @@
-$('a').click(() => {
-  console.log('fd');
+let theme = localStorage.getItem('theme') || 'dark';
+if (theme === 'light') {
+  $(':root').addClass('light-theme');
+  $('.icon').css('filter', 'invert(0)');
+  $('#wallpaper1,#wallpaper2').css('display', 'none');
+} else {
+  $(':root').removeClass('light-theme');
+  $('.icon').css(
+    'filter',
+    'brightness(0) invert(1) sepia(1) saturate(3) hue-rotate(140deg)'
+  );
+  $('#wallpaper1,#wallpaper2').css('display', 'block');
+}
+$('#hero-image-1, #hero-image-2').click(() => {
+  console.log('Theme toggle started');
+  if (theme === 'dark') {
+    theme = 'light';
+    $(':root').addClass('light-theme');
+    $('.icon').css('filter', 'invert(0)');
+    $('#wallpaper1,#wallpaper2').css('display', 'none');
+  } else {
+    theme = 'dark';
+    $(':root').removeClass('light-theme');
+    $('.icon').css(
+      'filter',
+      'brightness(0) invert(1) sepia(1) saturate(3) hue-rotate(140deg)'
+    );
+    $('#wallpaper1,#wallpaper2').css('display', 'block');
+  }
+
+  console.log('Theme changed to: ' + theme);
+  localStorage.setItem('theme', theme);
+  console.log('theme saved in localstorage');
 });
+$('.icon').hover(
+  function () {
+    if (theme === 'dark') {
+      $(this).css(
+        'filter',
+        'brightness(0) invert(1) sepia(1) saturate(3) hue-rotate(140deg)'
+      );
+    } else {
+      $(this).css('filter', 'brightness(0) invert(0)');
+    }
+  },
+  function () {
+    if (theme === 'dark') {
+      $(this).css('filter', 'invert(1)');
+    } else {
+      $(this).css('filter', 'none');
+    }
+  }
+);
